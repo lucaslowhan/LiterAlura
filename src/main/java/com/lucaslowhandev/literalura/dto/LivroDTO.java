966c68aeadf3ed7e28a27176a640a1ece442fe07
lucaslowhan <lucas.lowhan@hotmail.com>
2025-07-27 20:19:44 -0300

    feat: implementação de classes records

---
+++ src/main/java/com/lucaslowhandev/literalura/dto/LivroDTO.java
@@ -0,0 +1,11 @@
+package com.lucaslowhandev.literalura.dto;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+import java.util.List;
+
+public record LivroDTO(@JsonAlias("title") String titulo,
+                       @JsonAlias("languages") List<String> idiomas,
+                       @JsonAlias("authors") List<AutorDTO> autores,
+                       @JsonAlias("download_count") Double downloads) {
+}
